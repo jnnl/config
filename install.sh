11b0ce32c596d51ae2923ba6d7059de1fb39f3a8
Juho Junnila <juho@jnnl.net>
2016-06-10 14:05:33 +0300

    Add backup check and comments

--- install.sh
+++ install.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 # install.sh
 
+# Config file directory
 dir=~/.dotfiles
+# Config file backup directory
 backupdir=$dir/backup
+# Directory of this script
 scriptdir=$(cd "$(dirname "$1")"; pwd)/$(basename "$1")
+# Config files
 files="bashrc vimrc"
+# Operating system
 platform=$(uname)
 
+# Add bash_profile if running OS X
 if [[ $platform == "Darwin" ]]; then
 	files+=" bash_profile"
 fi
 
 cd ~
 
+# Check if config file directory exists
 if [[ -d "$dir" ]]; then
 	echo -n "$dir already exists. Continue? [y/N] "
 	read choice
@@ -22,18 +29,24 @@ if [[ -d "$dir" ]]; then
 	fi
 fi
 
+# Backup config files to the backup directory
 echo "Backing up existing config files"
 mkdir -p $backupdir
 
 for file in $files; do
 	if [[ -f ".$file" ]]; then
-		mv ~/.$file $backupdir/
+        if [[ -f "$backupdir/$file" ]]; then
+            mv ~/.$file $backupdir/$file.$(date +%Y%m%d%H%M%S)
+        else
+            mv ~/.$file $backupdir/$file
+        fi
 		if [[ "$?" -eq 0 ]]; then
 			echo "Moved .$file to $backupdir/"
 		fi
 	fi
 done
 
+# Copy config files from the repo to the config file directory
 echo "Copying config files to $dir"
 cd $scriptdir
 
@@ -44,6 +57,7 @@ for file in $files; do
 	fi
 done
 
+# Create symlinks of the config files to home directory
 echo "Linking config files to $HOME"
 cd $dir
 
