48de65a902fb677b4da5f42334036ef45f027e5e
Juho Junnila <juho@jnnl.net>
2016-06-22 00:30:54 +0300

    Retab to ts=4

--- install.sh
+++ install.sh
@@ -15,19 +15,19 @@ platform=$(uname)
 
 # Add bash_profile if running OS X
 if [[ $platform == "Darwin" ]]; then
-	files+=" bash_profile"
+    files+=" bash_profile"
 fi
 
 cd ~
 
 # Check if config file directory exists
 if [[ -d "$dir" ]]; then
-	echo -n "$dir already exists. Continue? [y/N] "
-	read choice
-	if echo "$choice" | grep -viq "^y" ;then
-		echo "No changes were made."
-		exit
-	fi
+    echo -n "$dir already exists. Continue? [y/N] "
+    read choice
+    if echo "$choice" | grep -viq "^y" ;then
+        echo "No changes were made."
+        exit
+    fi
 fi
 
 # Backup config files to the backup directory
@@ -35,16 +35,16 @@ echo "Backing up existing config files"
 mkdir -p $backupdir
 
 for file in $files; do
-	if [[ -f ".$file" ]]; then
+    if [[ -f ".$file" ]]; then
         if [[ -f "$backupdir/$file" ]]; then
             mv ~/.$file $backupdir/$file.$(date +%Y%m%d%H%M%S)
         else
             mv ~/.$file $backupdir/$file
         fi
-		if [[ "$?" -eq 0 ]]; then
-			echo "Moved ~/.$file to $backupdir/"
-		fi
-	fi
+        if [[ "$?" -eq 0 ]]; then
+            echo "Moved ~/.$file to $backupdir/"
+        fi
+    fi
 done
 
 # Copy config files from the repo to the config file directory
@@ -52,10 +52,10 @@ echo "Copying config files to $dir"
 cd $scriptdir
 
 for file in $files; do
-	cp $file $dir/
-	if [[ "$?" -eq 0 ]]; then
-		echo "Copied $file to $dir/$file"
-	fi
+    cp $file $dir/
+    if [[ "$?" -eq 0 ]]; then
+        echo "Copied $file to $dir/$file"
+    fi
 done
 
 # Create symlinks of the config files to home directory
@@ -64,9 +64,9 @@ cd $dir
 
 for file in $files; do
     ln -s $dir/$file ~/.$file
-	if [[ "$?" -eq 0 ]]; then
-		echo "Created symlink: $dir/$file -> ~/.$file"
-	fi
+    if [[ "$?" -eq 0 ]]; then
+        echo "Created symlink: $dir/$file -> ~/.$file"
+    fi
 done
 
 # Install vim-plug and colorschemes
