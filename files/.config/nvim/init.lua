61c7dba399db4b3d036ccef5d7f359e3ea6ebffe
Juho Junnila <git@jnnl.eu>
2023-01-25 13:12:19 +0200

    add lua-based neovim configs

---
+++ files/.config/nvim/init.lua
@@ -0,0 +1,196 @@
+-- init.lua
+
+-- Globals
+
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
+
+
+-- Plugins
+
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+    vim.notify('lazy.nvim not found, installing...', vim.log.levels.INFO)
+    vim.fn.system({ 'git', 'clone', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup('plugins', {
+    defaults = {
+        lazy = false,
+    },
+    lockfile = vim.fn.stdpath('data') .. '/lazy-lock.json',
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                "gzip",
+                "netrwPlugin",
+                "rplugin",
+                "tohtml",
+                "tutor",
+            },
+        },
+    },
+})
+
+
+-- General
+
+vim.opt.backspace = 'indent,eol,start'
+vim.opt.clipboard = 'unnamed'
+vim.opt.hidden = true
+vim.opt.joinspaces = false
+vim.opt.modeline = false
+vim.opt.mouse = ''
+vim.opt.shortmess:append('c')
+vim.opt.showcmd = true
+vim.opt.timeoutlen = 500
+
+
+-- Statusline
+
+vim.opt.laststatus = 2
+vim.opt.statusline = [[%f %y%m%=%l/%L]]
+
+
+-- Indentation
+
+vim.opt.autoindent = true
+vim.opt.breakindent = true
+vim.opt.expandtab = true
+vim.opt.shiftround = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = -1
+
+
+-- Search
+
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+
+-- Splits
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+
+-- Styles
+
+vim.opt.number = true
+if vim.fn.has('termguicolors') == 1 then
+    vim.opt.termguicolors = true
+end
+
+local ok, _ = pcall(vim.cmd.colorscheme, 'tonight')
+if not ok then
+    vim.notify('failed to load default colorscheme, using fallback...')
+    vim.cmd.colorscheme('desert')
+end
+
+
+-- Undo
+
+vim.opt.undodir = vim.fn.stdpath('state') .. '/undo'
+vim.opt.undofile = true
+
+
+-- Keymaps
+
+vim.keymap.set('n', '<BS>', '<C-^>')
+vim.keymap.set('n', '\'', '`')
+vim.keymap.set('n', '_', ',')
+vim.keymap.set('n', 'ö', '<C-o>')
+vim.keymap.set('n', 'ä', '<C-i>')
+
+vim.keymap.set('n', 'j', function() return vim.v.count == 0 and 'gj' or 'j' end, { expr = true })
+vim.keymap.set('n', 'k', function() return vim.v.count == 0 and 'gk' or 'k' end, { expr = true })
+vim.keymap.set({ 'n', 'x' }, '<C-j>', '}')
+vim.keymap.set({ 'n', 'x' }, '<C-k>', '{')
+vim.keymap.set('c', '<C-j>', '<Down>')
+vim.keymap.set('c', '<C-k>', '<Up>')
+
+vim.keymap.set('n', 'Q', '@q')
+vim.keymap.set('x', 'Q', '<cmd>normal @q<CR>')
+vim.keymap.set('x', '@', '<cmd>normal @')
+vim.keymap.set('x', '.', '<cmd>normal .<CR>')
+
+vim.keymap.set('n', '<Leader>s', function()
+    return ':%s/' .. vim.call('expand', '<cword>') .. '/'
+end, { expr = true })
+
+
+-- Commands
+
+vim.api.nvim_create_user_command('Rstrip', ':%s/\\s\\+$//e', { bang = true })
+vim.api.nvim_create_user_command('Unansify', ':%s/\\%x1b\\[[0-9;]*[a-zA-Z]//ge', { bang = true })
+vim.api.nvim_create_user_command('NonAscii', '/[^\\x00-\\x7F]', { bang = true })
+
+-- Autocommands
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'make',
+    callback = function()
+        vim.opt_local.noexpandtab = true
+        vim.opt_local.shiftwidth = 8
+    end
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'vim', 'help' },
+    callback = function()
+        vim.opt_local.keywordprg = ':help'
+    end
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'css', 'scss', 'html', 'typescript' },
+    callback = function()
+        local create_cmd = function(name, command)
+            vim.api.nvim_create_user_command(name, command, { bang = true })
+        end
+        create_cmd('ET', ':e %:p:r.html')
+        create_cmd('EC', ':e %:p:r.ts')
+        create_cmd('ES', ':e %:p:r.scss')
+        create_cmd('ST', ':sp %:p:r.html')
+        create_cmd('SC', ':sp %:p:r.ts')
+        create_cmd('SS', ':sp %:p:r.scss')
+        create_cmd('VT', ':vs %:p:r.html')
+        create_cmd('VC', ':vs %:p:r.ts')
+        create_cmd('VS', ':vs %:p:r.scss')
+    end
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'go',
+    callback = function()
+        vim.opt_local.noexpandtab = true
+        vim.opt_local.shiftwidth = 8
+        local create_cmd = function(name, command)
+            vim.api.nvim_create_user_command(name, command, { bang = true })
+        end
+        create_cmd('EC', ':e %:p:s?_test.go?.go?')
+        create_cmd('SC', ':vs %:p:s?_test.go?.go?')
+        create_cmd('VC', ':sp %:p:s?_test.go?.go?')
+        create_cmd('ET', ':e %:p:r_test.go')
+        create_cmd('ST', ':sp %:p:r_test.go')
+        create_cmd('VT', ':vs %:p:r_test.go')
+    end
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        require('vim.highlight').on_yank({ higroup = 'IncSearch', timeout = 1000, on_visual = false })
+    end
+})
+
+vim.api.nvim_create_autocmd({ 'BufWritePre', 'FileWritePre' }, {
+    callback = function()
+        local dir = vim.fn.expand('<afile>:p:h')
+        if vim.fn.isdirectory(dir) == 0 then
+            vim.fn.system({ 'mkdir', '-vp', dir })
+        end
+    end
+})
---
+++ files/.config/nvim/lua/plugins.lua
@@ -0,0 +1,264 @@
+return {
+    -- Navigation
+    { 'andymass/vim-matchup',
+      config = function()
+          vim.g.matchup_matchparen_offscreen = { method = 'popup' }
+      end
+    },
+    { 'ggandor/leap.nvim',
+      config = function()
+          require('leap').add_default_mappings()
+      end
+    },
+    { 'junegunn/fzf', build = './install --xdg --key-bindings --completion --no-fish --no-zsh --no-update-rc' },
+    { 'junegunn/fzf.vim',
+      config = function()
+          vim.keymap.set('n', '<Leader>,', '<cmd>Files<CR>', { silent = true })
+          vim.keymap.set('n', '<Leader>.', '<cmd>Buffers<CR>', { silent = true })
+          vim.keymap.set('n', '<Leader>-', '<cmd>Ripgrep<CR>', { silent = true })
+          vim.keymap.set('n', '<Leader>;', '<cmd>History<CR>', { silent = true })
+          vim.keymap.set('n', '<Leader>:', '<cmd>BCommits<CR>', { silent = true })
+          vim.keymap.set('n', '<Leader>_', '<cmd>GitRipgrep<CR>', { silent = true })
+          vim.keymap.set('n', '<Leader>\'', '<cmd>Files ~<CR>', { silent = true })
+          vim.keymap.set('n', '<Leader>*', '<cmd>GitFiles<CR>', { silent = true })
+          vim.cmd([[ command! -nargs=* Ripgrep
+              \ call fzf#vim#grep(
+              \ 'rg --column --line-number --no-heading --smart-case '
+              \ . '--color=always --colors "path:fg:green" --colors "line:fg:yellow" '
+              \ . shellescape(<q-args>), 1,
+              \ fzf#vim#with_preview({ 'options': '--delimiter : --nth 4..' }, 'right:50%:hidden', 'ctrl-/'))
+          ]])
+          vim.cmd([[ command! -bang -nargs=* GitRipgrep
+              \ call fzf#vim#grep(
+              \ 'rg --column --line-number --no-heading  --smart-case '
+              \ . '--color=always --colors "path:fg:green" --colors "line:fg:yellow" '
+              \ . shellescape(<q-args>), 1,
+              \ fzf#vim#with_preview(
+              \ { 'options': '--delimiter : --nth 4..',
+              \   'dir': system('git -C ' . expand('%:p:h') . ' rev-parse --show-toplevel 2>/dev/null')[:-2]
+              \ }, 'right:50%:hidden', 'ctrl-/'),
+              \ <bang>0)
+          ]])
+      end
+    },
+    { 'romainl/vim-cool' },
+
+    -- Manipulation
+    { 'tommcdo/vim-exchange' },
+    { 'tommcdo/vim-lion',
+      config = function()
+        vim.g.lion_squeeze_spaces = 1
+      end
+    },
+    { 'tpope/vim-abolish' },
+    { 'tpope/vim-commentary' },
+    { 'tpope/vim-surround' },
+
+    -- Colorschemes
+    { 'jnnl/vim-tonight' },
+
+    -- Language
+    { 'ap/vim-css-color',
+      ft = { 'css', 'scss' },
+    },
+    { 'hashivim/vim-terraform' },
+    { 'leafgarland/typescript-vim' },
+    { 'prettier/vim-prettier',
+      ft = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue', 'svelte', 'css', 'scss', 'html' },
+      config = function()
+          vim.keymap.set('n', '<Leader>p', '<cmd>Prettier<CR>', { silent = true })
+          vim.g['prettier#autoformat_config_present'] = 1
+          vim.g['prettier#autoformat_require_pragma'] = 0
+      end
+    },
+    { 'ziglang/zig.vim' },
+
+    -- LSP
+    { 'neovim/nvim-lspconfig',
+      config = function()
+          local cmp_nvim_lsp = require('cmp_nvim_lsp')
+          local lsp = require('lspconfig')
+
+          local local_on_attach = function(_, bufnr)
+              vim.diagnostic.config({ virtual_text = false })
+              vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+              vim.cmd('command! Format execute "lua vim.lsp.buf.format({ async = true })"')
+
+              local opts = { noremap = true, silent = true, buffer = bufnr }
+
+              vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+              vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+              vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+              vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+              vim.keymap.set('n', 'gö', '<cmd>lua vim.diagnostic.goto_prev({severity = {min = vim.diagnostic.severity.WARN}})<CR>', opts)
+              vim.keymap.set('n', 'gä', '<cmd>lua vim.diagnostic.goto_next({severity = {min = vim.diagnostic.severity.WARN}})<CR>', opts)
+              vim.keymap.set('n', '<Space>', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+              vim.keymap.set('n', '<C-Space>', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+              vim.keymap.set('n', '<leader><Space>', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+              vim.keymap.set('x', '<leader><Space>', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
+              vim.keymap.set('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+              vim.keymap.set('n', '<leader>f', '<cmd>Format<CR>', opts)
+              vim.keymap.set('n', '<leader>tt', '<cmd>TroubleToggle workspace_diagnostics<CR>', opts)
+              vim.keymap.set('n', '<leader>tr', '<cmd>TroubleToggle lsp_references<CR>', opts)
+          end
+
+          local capabilities = cmp_nvim_lsp.default_capabilities()
+          capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+          local servers = { 'bashls', 'cssls', 'gopls', 'html', 'pyright', 'rust_analyzer', 'tsserver' }
+
+          for _, server in ipairs(servers) do
+              if server == 'tsserver' then
+                  require('typescript').setup({
+                      server = {
+                          capabilities = capabilities,
+                          on_attach = function(client, bufnr)
+                              local_on_attach(client, bufnr)
+                          end
+                      }
+                  })
+              else
+                  lsp[server].setup({
+                      capabilities = capabilities,
+                      on_attach = local_on_attach,
+                  })
+              end
+          end
+
+      end
+    },
+    { 'folke/trouble.nvim',
+      config = function()
+          require('trouble').setup({
+              icons = false,
+              fold_open = 'v',
+              fold_closed = '>',
+              indent_lines = false,
+              signs = {
+                  error = '[error]',
+                  warn = '[warn]',
+                  hint = '[hint]',
+                  information = '[info]',
+                  other = '[other]',
+              },
+              use_diagnostic_signs = false
+          })
+      end
+    },
+    { 'jose-elias-alvarez/typescript.nvim' },
+
+    -- Completion
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-nvim-lua' },
+    { 'hrsh7th/cmp-path' },
+    { 'hrsh7th/cmp-vsnip' },
+    { 'hrsh7th/vim-vsnip',
+      config = function()
+          -- vim.g.vsnip_filetypes = { 'html' }
+          vim.g.vsnip_snippet_dir = vim.fn.stdpath('config') .. '/snippets'
+      end
+    },
+    { 'hrsh7th/vim-vsnip-integ' },
+    { 'hrsh7th/nvim-cmp',
+      dependencies = {
+          'hrsh7th/cmp-nvim-lsp',
+          'hrsh7th/cmp-path',
+          'hrsh7th/cmp-nvim-lua',
+          'hrsh7th/cmp-vsnip',
+          'hrsh7th/vim-vsnip',
+          'hrsh7th/vim-vsnip-integ',
+      },
+      config = function()
+          vim.opt.completeopt = 'menu,menuone,noselect'
+	  local cmp = require('cmp')
+          cmp.setup({
+              snippet = {
+                  expand = function(args)
+                      vim.fn['vsnip#anonymous'](args.body)
+                  end
+              },
+              mapping = {
+                  ['<C-k>'] = cmp.mapping.select_prev_item(),
+                  ['<C-j>'] = cmp.mapping.select_next_item(),
+                  ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+                  ['<C-u>'] = cmp.mapping.scroll_docs(4),
+                  ['<C-Space>'] = cmp.mapping.complete(),
+                  ['<C-e>'] = cmp.mapping.close(),
+                  ['<CR>'] = cmp.mapping.confirm {
+                      behavior = cmp.ConfirmBehavior.Replace,
+                      select = true,
+                  },
+                  ['<Tab>'] = cmp.mapping(function(fallback)
+                      if cmp.visible() then
+                          cmp.select_next_item()
+                      elseif vim.fn['vsnip#available'](1) == 1 then
+                          vim.api.nvim_feedkeys(
+                            vim.api.nvim_replace_termcodes('<Plug>(vsnip-expand-or-jump)', true, true, true),
+                            '', true
+                          )
+                      else
+                          fallback()
+                      end
+                  end, { 'i', 's' }),
+                  ['<S-Tab>'] = cmp.mapping(function(fallback)
+                      if cmp.visible() then
+                          cmp.select_prev_item()
+                      elseif vim.fn['vsnip#jumpable'](-1) == 1 then
+                          vim.api.nvim_feedkeys(
+                             vim.api.nvim_replace_termcodes('<Plug>(vsnip-jump-prev)', true, true, true),
+                             '', true
+                          )
+                      else
+                          fallback()
+                      end
+                  end, { 'i', 's' }),
+              },
+              sources = {
+                  { name = 'nvim_lsp' },
+                  { name = 'nvim_lua' },
+                  { name = 'vsnip' },
+                  { name = 'path' },
+              }
+          })
+
+      end
+    },
+    { 'ray-x/lsp_signature.nvim',
+      config = function()
+          require('lsp_signature').setup({
+              bind = true,
+              hint_enable = false,
+              handler_opts = {
+                  border = 'single',
+              },
+          })
+      end
+    },
+
+    -- Miscellaneous
+    { 'editorconfig/editorconfig-vim' },
+    { 'folke/which-key.nvim',
+      config = function()
+          require('which-key').setup({})
+      end
+    },
+    { 'mbbill/undotree',
+      config = function()
+        vim.keymap.set('n', '<Leader>u', '<cmd>UndotreeToggle<CR>', { silent = true })
+        vim.g.undotree_SetFocusWhenToggle = 1
+        vim.g.undotree_ShortIndicators = 1
+        vim.g.undotree_HelpLine = 0
+      end
+    },
+    { 'michaeljsmith/vim-indent-object' },
+    { 'nvim-lua/plenary.nvim' },
+    { 'romainl/vim-qf',
+      config = function()
+        vim.keymap.set('n', 'Ö', '<Plug>(qf_qf_previous)')
+        vim.keymap.set('n', 'Ä', '<Plug>(qf_qf_next)')
+      end
+    },
+    { 'tpope/vim-repeat' },
+    { 'tpope/vim-fugitive' },
+    { 'whiteinge/diffconflicts' },
+}
