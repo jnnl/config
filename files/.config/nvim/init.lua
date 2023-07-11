1ffcc07d3af442e59a9e295086361958dd3fc5a3
Juho Junnila <git@jnnl.eu>
2023-07-11 23:59:59 +0300

    add more nvim user commands

--- files/.config/nvim/init.lua
+++ files/.config/nvim/init.lua
@@ -161,6 +161,13 @@ vim.api.nvim_create_user_command('CloseFloatingWindows', function()
         end
     end
 end, { bang = true })
+vim.api.nvim_create_user_command('DiffChanges', ':w !git diff --no-index % -', { bang = true })
+vim.api.nvim_create_user_command('Redir', function(ctx)
+  local lines = vim.split(vim.api.nvim_exec(ctx.args, true), '\n', { plain = true })
+  vim.cmd('new')
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  vim.opt_local.modified = false
+end, { nargs = '+', complete = 'command' })
 
 -- Autocommands
 
